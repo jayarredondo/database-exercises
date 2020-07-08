45907a2c5443d4077c0bbb06a0fb91c4ea513558
Jay Arredondo <jearredondo13@gmail.com>
2020-07-08 11:30:54 -0500

    finished order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,10 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name DESC;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name DESC;
 
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
@@ -16,7 +20,9 @@ SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 SELECT * FROM employees WHERE birth_date LIKE '%12-25';
 
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25';
+SELECT * FROM employees WHERE hire_date
+    BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%12-25' ORDER BY birth_date, hire_date DESC;
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
