504ff0cd77cdb728612893187b077ed4304c825d
Jay Arredondo <jearredondo13@gmail.com>
2020-07-09 09:12:33 -0500

    finished functions exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' ORDER BY emp_no DESC
+
+SELECT *, CONCAT('Days at company: ', DATEDIFF(NOW(), hire_date))
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date desc;
